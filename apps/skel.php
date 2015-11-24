91086e91c0a57ddd31b22e765368e1fb5d7b7a65
lambertmeyer <meyer.lambert68@gmail.com>
2015-11-24 16:34:53 +0100

    hhh

--- apps/skel.php
+++ apps/skel.php
@@ -1,4 +1,12 @@
 <?php
 $titre = "ShadowPikachu";
+
+if (isset($_GET['ajax']))
+{
+	require('apps/chat_message.php');	
+}
+else {
+	
 require('views/skel.phtml');
+}
 ?>
--- public/script/script.js
+++ public/script/script.js
@@ -1,5 +1,13 @@
 $('.chat').submit(function(e){
     e.preventDefault(); // on empêche le bouton d'envoyer le formulaire
+    $.post('index.php?page=chat', {"content":$('#message').val()}, function(page)
+    {
+        
+        $.get('index.php?page=chat&ajax', function(page)
+        {
+            $('.chat_home').html(page);
+        });
 
-   
+
+    });
 });
