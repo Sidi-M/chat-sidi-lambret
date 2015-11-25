b9d1c4b181494a156fa796648e8341e1d12de433
lambertmeyer <meyer.lambert68@gmail.com>
2015-11-25 09:22:11 +0100

    hhh

--- public/script/script.js
+++ public/script/script.js
@@ -1,13 +1,30 @@
+function refresh()
+{
+    $.get('index.php?page=chat&ajax', function(page)
+    {
+        $('.chat_home').html(page);
+    });
+}
+
+
 $('.chat').submit(function(e){
     e.preventDefault(); // on empêche le bouton d'envoyer le formulaire
     $.post('index.php?page=chat', {"content":$('#message').val()}, function(page)
     {
-        
-        $.get('index.php?page=chat&ajax', function(page)
-        {
-            $('.chat_home').html(page);
-        });
+        refresh();
+        // function charger(){
+        //     setTimeout( function(){
+
+                
+
+    //         charger();
+    //     }, 2000);
+    // }
+    // charger();
+
 
 
     });
 });
+
+setInterval(refresh, 1000);
