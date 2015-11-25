39bf415645108fbe4139dcc649dcbea9150e954e
lambertmeyer <meyer.lambert68@gmail.com>
2015-11-25 11:43:30 +0100

    hhh

--- apps/chat_message.php
+++ apps/chat_message.php
@@ -1,8 +1,19 @@
 <?php
 $query = "SELECT * FROM message LEFT JOIN users ON users.id=message.id_auteur ORDER BY message.id DESC LIMIT 5";
 $mtchat = mysqli_query($database, $query);
+$listmessage = array();
 while ($message = mysqli_fetch_assoc($mtchat))
 {
+	$listmessage[] = $message;
+}
+	$listmessage = array_reverse($listmessage);
+
+
+	$i = 0;
+while ( isset($listmessage[$i])) 
+{
+	$message = $listmessage[$i];
 	require('views/chat_message.phtml');
+	$i++;
 }
 ?>
